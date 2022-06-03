4b78c75e782f263c38a44257d9006466eb81031b
nar2p1nk <farrisisqandar@gmail.com>
2022-06-03 15:11:50 +0800

    added two new ul, one for completed and other for uncompleted todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {useEffect, useState} from "react";
+import './App.css';
 
 function App() {
 
@@ -13,11 +14,37 @@ function App() {
   },[])
   return (
     <div className="App">
-      {todo.map((item)=>{
-        return(
-          <p>{item.todo}</p>
-        )
-      })}
+      <h1 className="title">Todo-app</h1>
+      <form >
+        <input type="text" placeholder />
+        <button>Create todo</button>
+      </form>
+      <div className='uncomplete'>
+      <ul className="todoList">
+        {todo.map(i=>{
+          if(i.completed === 1){
+            return(null);
+          }
+            return(
+              <li id={i.todoId} key={i.todoId}>{i.todo}</li>
+            )
+        })}
+      </ul>
+      </div>
+      <div className='complete'>
+        <h3>completed:</h3>
+      <ul>
+        {todo.map(i=>{
+          // filter uncompleted todo
+          if(i.completed === 0){
+            return(null);
+          }
+            return(
+              <li id={i.todoId} key={i.todoId}>{i.todo}</li>
+            )
+        })}
+      </ul>
+      </div>
     </div>
   );
 }
